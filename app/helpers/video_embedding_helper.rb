56146a58b0bf3d9e2a08b64a8239200bb77ba5ac
Antriksh <antriksh.scin9@gmail.com>
2024-03-22 17:12:58 +0530

    updated-logic

--- app/helpers/video_embedding_helper.rb
+++ app/helpers/video_embedding_helper.rb
@@ -2,41 +2,51 @@
 # your document. 
 # ::usage:: vid(youtube, 123456)
 module VideoEmbeddingHelper
-
-class VideoMarkdownRenderer < Redcarpet::Render::HTML
+  class VideoMarkdownRenderer < Redcarpet::Render::HTML
     include ActionView::Helpers::AssetTagHelper
-  
+
     def preprocess(document)
-        # scans for: vid(provider, id)
-        # e.g. vid(youtube,12345)
-        vidregex = Regexp.compile(/\bvid\((\S+)\,\s*(\S+)\)\s*/)
-        document.to_enum(:scan, vidregex).map { Regexp.last_match }.each do |match|
-            document.gsub!(match.to_s, video_div(match[1], match[2]))
-        end
-        document
-      end      
-  
-    def video_div(provider, vid)
-      content_tag(:div, 
-        iframe_for(provider, vid), class: 'embed-responsive embed-responsive-16by9' 
+      document.gsub!(youtube_regex) do |match|
+        youtube_video_embed($1)
+      end
+
+      document.gsub!(vimeo_regex) do |match|
+        vimeo_video_embed($1)
+      end
+
+      document
+    end
+
+    private
+
+    def youtube_regex
+      %r{(?:https?://)?(?:www\.)?(?:youtube\.com/watch\?v=|youtu\.be/|youtube\.com/embed/|youtube\.com/v/|youtube\.com/user/.+/u/|youtube\.com/attribution_link\?.*v%3D|youtube\.googleapis\.com/v/)([a-zA-Z0-9_-]+)(?:\?[^'"\s]+)?}
+    end
+
+    def vimeo_regex
+      %r{(https?://(?:www\.)?vimeo\.com/(\d+))}
+    end
+
+    def youtube_video_embed(video_id)
+      content_tag(:iframe, nil, 
+                  width: '100%', 
+                  height: 'auto', 
+                  src: "https://www.youtube.com/embed/#{video_id}?autoplay=1", 
+                  class: 'embed-responsive-item',
+                  allowfullscreen: true,
+                  frameborder: 1
       )
     end
-    def iframe_for(provider, vid)
-      case provider 
-      when 'youtube'
-        return content_tag(:iframe, nil, 
-                           width: '100%', 
-                           height: 'auto', 
-                           src: "https://www.youtube.com/embed/#{vid}", 
-                           class: 'embed-responsive-item')
-      when 'vimeo'
-        return content_tag(:iframe, nil, 
-                           width: '100%', 
-                           height: 'auto', 
-                           src: "https://player.vimeo.com/video/#{vid}", 
-                           class: 'embed-responsive-item')
-
-    end
-    end
+    
+    def vimeo_video_embed(video_id)
+      content_tag(:iframe, nil, 
+                  width: '100%', 
+                  height: 'auto', 
+                  src: "https://player.vimeo.com/video/#{video_id}", 
+                  class: 'embed-responsive-item',
+                  allowfullscreen: true,
+                  frameborder: 1
+      )
+    end        
   end
-end
+end  
