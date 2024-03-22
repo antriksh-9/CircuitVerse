6e0e6ade888ca510f76050fbfcf3b5ae65c1080b
Antriksh <antriksh.scin9@gmail.com>
2024-03-22 17:08:23 +0530

    updated-logic

--- app/helpers/circuit_embedding_helper.rb
+++ app/helpers/circuit_embedding_helper.rb
@@ -1,14 +1,11 @@
-# circuit_embedding_helper.rb
-
 module CircuitEmbeddingHelper
-    
     class CircuitMarkdownRenderer < Redcarpet::Render::HTML
       include ActionView::Helpers::AssetTagHelper
-    
+  
       def preprocess(document)
-        # Scan for circuit tags and replace them with embedded circuit iframes
-        # e.g., circuit(sap-1-a993d26d-99c4-4fb9-a5ce-06f38f44eb98)
-        circuit_regex = /\bcircuit\((\S+)\)/
+        # Scan for CircuitVerse circuit URLs and replace them with embedded iframes
+        # e.g., https://circuitverse.org/simulator/embed/binary-to-bcd-f683ef45-c8cb-4432-b4b4-d9e131cab048
+        circuit_regex = %r{https?://circuitverse\.org/simulator/embed/([a-zA-Z0-9-]+)}
         document.gsub!(circuit_regex) do |match|
           circuit_id = $1
           circuit_div(circuit_id)
@@ -17,22 +14,21 @@ module CircuitEmbeddingHelper
       end
   
       def circuit_div(circuit_id)
-        # Generate HTML for embedding a circuit based on the circuit ID
+        # Generate HTML for embedding a CircuitVerse circuit based on the circuit ID
         content_tag(:iframe,
-          nil,
-          style: 'border-width:; border-style: solid; border-color:;',
-          width: '500',
-          name: 'myiframe',
-          height: '500',
-          scrolling: 'no',
-          id: 'projectPreview',
-          frameborder: '1',
-          marginheight: '0px',
-          marginwidth: '0px',
-          src: "https://circuitverse.org/simulator/embed/#{circuit_id}?theme=default&display_title=false&clock_time=true&fullscreen=true&zoom_in_out=true",
-          allowfullscreen: 'true'
+                    nil,
+                    style: 'border-width:; border-style: solid; border-color:;',
+                    width: '500',
+                    name: 'myiframe',
+                    height: '500',
+                    scrolling: 'no',
+                    id: 'projectPreview',
+                    frameborder: '1',
+                    marginheight: '0px',
+                    marginwidth: '0px',
+                    src: "https://circuitverse.org/simulator/embed/#{circuit_id}?theme=default&display_title=false&clock_time=true&fullscreen=true&zoom_in_out=true",
+                    allowfullscreen: 'true'
         )
       end
     end
-  end
-  
+end  
